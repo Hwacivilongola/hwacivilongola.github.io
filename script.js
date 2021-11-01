80b13d24d0a55434596403cb2dcd96d13cc944a2
AlvinBalvin321 <alvinbalvin321@gmail.com>
2021-10-31 22:45:05 -0400

    Edited all files

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var counter = 0;
+var counter = localStorage.getItem("counter");
 function counterUp () {
     counter++;
+    localStorage.setItem("counter", counter);
     document.getElementById("counter").innerHTML = counter;
+}
+
+function counterDouble () {
+    counter *= 2;
+    localStorage.setItem("counter", counter);
+    document.getElementById("counter").innerHTML = counter;
+}
+
+function counterZero () {
+    if (confirm("THIS WILL SET IT ALL TO ZERO, CANNOT BE UNDONE, ARE YOU SURE?")){
+        if (confirm("ARE YOU SURE SURE???")) {
+        counter = 0;
+        localStorage.setItem("counter", counter);
+        document.getElementById("counter").innerHTML = counter;
+        }
+    }
 }
