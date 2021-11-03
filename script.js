61747edede5a6bec4d6c9f15b385fd23f2cf7565
AlvinBalvin321 <alvinbalvin321@gmail.com>
2021-11-02 22:52:38 -0400

    Stuff

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 var counter = localStorage.getItem("counter");
 
-function counterUp () {
+function counterUp() {
     counter++;
-    localStorage.setItem("counter", counter);
-    document.getElementById("counter").innerHTML = counter;
+    counterLoad();
 }
 
-function counterDouble () {
+function counterDouble() {
     counter *= 2;
-    localStorage.setItem("counter", counter);
-    document.getElementById("counter").innerHTML = counter;
+    counterLoad();
 }
 
-function counterZero () {
+function counterZero() {
     if (confirm("THIS WILL SET IT ALL TO ZERO, CANNOT BE UNDONE, ARE YOU SURE?")){
         if (confirm("ARE YOU SURE SURE???")) {
         counter = 0;
-        localStorage.setItem("counter", counter);
-        document.getElementById("counter").innerHTML = counter;
+        counterLoad();
         }
     }
 }
 
-function counterLoad () {
+function counterLoad() {
+    localStorage.setItem("counter", counter);
     document.getElementById("counter").innerHTML = counter;
+
+    if (counter==69) {
+        document.getElementById("counter").innerHTML = "nice";
+    } else if (counter=420) {
+        document.getElementById("counter").innerHTML = "weed";
+    } else if (counter==69420) {
+        document.getElementById("counter").innerHTML = "VERY NICE!";
+    }
 }
