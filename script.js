1c6e1531b62f37b9e7a2fb809280171674e3ae9a
AlvinBalvin321 <alvinbalvin321@gmail.com>
2021-11-28 20:14:21 -0500

    edited aboutme and lifestory, fixed the counter, and made buttosn look nice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var counter = localStorage.getItem("counter");
-var score = 0;
+var score = localStorage.getItem("score");
 
 function loadEverything() {
     console.log("loaded begun")
@@ -8,24 +8,31 @@ function loadEverything() {
         document.body.classList.toggle("darkmode");
         console.log("toggled dark mode")
     }
+    document.getElementById("score").innerHTML = "";
     console.log("loaded everything")
 }
 
 function counterLoad() {
-    counter = localStorage.getItem("counter");
-    score = 0;
     localStorage.setItem("counter", counter);
-    //document.getElementById("counter").innerHTML = counter;
+    localStorage.setItem("score", score);
+    counter = localStorage.getItem("counter");
+    document.getElementById("counter").innerHTML = counter;
     
     if (counter==69) {
         document.getElementById("counter").innerHTML = "nice";
         document.getElementById("score").innerHTML = "score for getting 69: "+score+" clicks";
-    } else if (counter==420) {
-        document.getElementById("counter").innerHTML = "weed";
-        document.getElementById("score").innerHTML = "score for getting 420: "+score+" clicks";
     } else if (counter==69420) {
         document.getElementById("counter").innerHTML = "VERY NICE!";
         document.getElementById("score").innerHTML = "score for getting 62420: "+score+" clicks";
+    } else if (counter==70) {
+        document.getElementById("counter").innerHTML = "avo's fav numby";
+        document.getElementById("score").innerHTML = "score for getting 70: "+score+" clicks";
+    } else if (counter==Infinity) {
+        document.getElementById("counter").innerHTML = "did u cheat to get infinity?";
+        document.getElementById("score").innerHTML = "score for getting infinity: "+score+" clicks";
+    } else if (counter<0) {
+        document.getElementById("counter").innerHTML = "how- i- im not even gonna question it";
+        document.getElementById("score").innerHTML = "score for getting a negative number: "+score+" clicks";
     }
 }
 
